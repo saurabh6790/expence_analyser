b30487b809a3430f39d6ff2441a7410d46cb4b59
rohitwaghchaure <rohitw1991@gmail.com>
2016-03-26 16:39:06 +0530

    new ch

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template,  request, redirect, url_for
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -23,13 +23,25 @@ def login():
 		cursor.execute("""select `username` from Auth where `username`='{0}'
 			and `pwd`=password('{1}')""".format(username, password))
 		data = cursor.fetchone()
+		print data
 		if data is None:
 			error = "Username or Password is wrong"
 		else:
 			return render_template("welcome.html")
 	
 	return render_template("login.html", error=error)
-	
+
+@app.route("/signup.html", methods=["GET", "POST"])
+def signup():
+	if request.method == "POST":
+		cursor = mysql.connect().cursor()
+		cursor.execute("""insert into `User` values('{0}', '{1}', '{2}', '{3}') 
+			""".format(request.form['username'], request.form['first_name'], request.form['last_name'], request.form['mobile_no']))
+		cursor.execute("""insert into Auth values('{0}', password('{1}')) 
+			""".format(request.form['username'], request.form['password']))
+		cursor.execute("commit")
+		return login()
+	return render_template("signup.html")
 	
 if __name__ == "__main__":
 	app.run(debug=True)
